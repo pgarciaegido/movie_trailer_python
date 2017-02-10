19e5d1bc148059eb2796b3b96089dda82b766b18
Pablo Garcia Egido <pgarciaegido@gmail.com>
2017-02-10 13:13:07 +0100

    Fixing implementation and error handling

--- imdb_request.py
+++ imdb_request.py
@@ -1,12 +1,28 @@
 import requests
 import json
 
+# Gets movie information from OMDB API.
 def get_movie_json(movie):
+
+    # Sends this JSON in case of error.
+    default_json_error = {"Plot": "Description not available at the moment. Sorry :(",
+                          "Poster": "https://image.ibb.co/iUi5va/error.jpg",
+                          "imdbRating": "N/A"}
+
+    # Formats movie title so it can be read properly by the API.
     title = movie.title.replace(' ', '+').lower()
     try:
+        # Tries to make a GET HTTP request. If success, return JSON.
         r = requests.get('http://www.omdbapi.com/?t=' + title + '&y=&plot=short&r=json')
         movie = r.json()
-        return movie
+
+        # If the response is false, return error JSON
+        if movie["Response"] == "False":
+            return default_json_error
+        else:
+            return movie
+
     except:
+        # If request fails, prints an error alert in console and returns default fail info
         print('There is been an error')
-        return {"imdbID":"", "imdbRating": "N/A"}
+        return default_json_error
--- media.py
+++ media.py
@@ -1,10 +1,7 @@
 import webbrowser
 
 class Movie():
-    """ This class provides a way to store information about movies """
-
-    """ Constructor takes information of the movie. A title, a brief description,
-    a poster image and the trailer url from Youtube. """
+    """ Constructor takes information of the movie. A title and the trailer url from Youtube. """
 
     def __init__(self, movie_title, trailer_youtube):
         self.title = movie_title
