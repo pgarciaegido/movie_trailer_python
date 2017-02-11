35534b7c3de4d1c1860082e6d2c60eb339c4cf96
Pablo Garcia Egido <pgarciaegido@gmail.com>
2017-02-11 14:15:01 +0100

    Improving error handling on API request

--- imdb_request.py
+++ imdb_request.py
@@ -10,13 +10,13 @@ def get_movie_json(movie):
                                   " Sorry :(",
                           "imdbRating": "N/A"}
 
-    # Sends this message in case of error.
-    error_message = ("It looks that the movie --> " + movie.title +
-                    " is not being recognized by the API or the server is "
-                    "not responding.\nPlease double check if the name "
-                    "introduced is correctly spelled.\nIf the title is not "
-                    "english, it might be recorded under the english name \n"
-                    "Check http://www.omdbapi.com/ for further information.")
+    # Sends this message in case of ValueError.
+    value_error_message = ("It looks that the movie --> " + movie.title +
+                    " is not being recognized by the API.\nPlease double "
+                    "check if the name introduced is correctly spelled.\n"
+                    "If the title is not english, it might be recorded under "
+                    "the english name \nCheck http://www.omdbapi.com/ "
+                    "for further information.")
 
     # Formats movie title so it can be read properly by the API.
     title = movie.title.replace(' ', '+').lower()
@@ -25,15 +25,18 @@ def get_movie_json(movie):
         r = requests.get('http://www.omdbapi.com/?t=' + title + '&y=&plot=short&r=json')  # NOQA
         movie = r.json()
 
-        # If the response is false, returns error JSON
+        # If the response is false, raises an exception
         if movie["Response"] == "False":
-            print(error_message)
-            return default_json_error
+            raise ValueError(value_error_message)
         else:
             return movie
 
-    except:
-        # If request fails, prints an error alert in console and
-        #returns error JSON
-        print(error_message)
+    except requests.exceptions.ConnectionError as e:
+        # If request fails because there is a Connection Error
+        print(e, '\nThere is been a problem with your connection.\n')
+        return default_json_error
+
+    except ValueError as e:
+        # If movie title not recognized by the API.
+        raise
         return default_json_error
